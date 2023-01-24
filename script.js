0617a67a7d125f7a527343e74c024fd9305c0921
sumitssaurabh <117895961+sumitssaurabh@users.noreply.github.com>
2023-01-25 02:09:24 +0530

    12

--- script.js
+++ script.js
@@ -1,1 +1,72 @@
-//your code here
+
+
+// // create h3 element
+
+// let heading = document.createElement("h3");
+// h3.setAttribute("id" , "h")
+// h3.innerHTML = "please click on the identical tiles to verify that you are not a robot."
+// document.body.appendChild(h3);
+
+// //  create btn
+// let btn = document.createElement("button")
+// btn.setAttribute("id","resert")
+// btn.innerHTML = ("id","reset")
+// document.body.append(btn)
+
+// // // generating 6thclass name randomly 
+// // from 5 values
+
+// let imgClass =["img1","img2", "img3","img4","img5"]
+// let randon = Math.floor (Math.random() * imgClass.length)
+
+// let randomImg = imgClass[random]
+// imgClass.push(randomimg)
+
+
+// // shuffling array 
+// let arr1 = [];
+// let k = 0;
+
+// while(true){
+//     let random2 = Math.floor(Math.random() * imgClass.length ) //
+//     if(arr1[random2]==undefined){
+//         arr1[random2] = imgClass[k]
+//         k =k++;
+//     }
+//     else if(arr1[random2] != undefined){
+//         continue;
+//     }
+    
+    
+    
+// }
+
+ 
+
+
+
+
+
+
+// //  selecting all image 
+// let images = documnet.querySelectorAll("img")
+
+// // setting attibute - class Name to all images
+
+// for(let i=0; i<=images.length-1; i++){
+//     image[i].setAttribute("class", imgClass[i]);
+    
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
